7d264a31068e38869e0d78441509f282e2cebc69
sndnyang <xiulong.yancey.yang@gmail.com>
2019-09-17 01:09:47 -0400

    update

--- build.sh
+++ build.sh
@@ -11,6 +11,11 @@ sed '/#body#/{
     r body.tex
     }' $dir/template.tex > $dir/main.tex
 
+if [[ $dir == "cvpr" ]]
+then
+    sed -i "/newcommand{.eg/d;/newcommand{.ie/d" $dir/main.tex
+fi
+
 if [[ $dir == 'iclr' ]]
 then
     sed -i '/newcommand{\\vs/d;/newcommand{\\ve/d' $dir/main.tex
@@ -18,6 +23,11 @@ fi
 
 cp $dir/main.tex main_$dir.tex
 cp ref.bib $dir/
+cp -r figures $dir/
+
+# pdflatex -synctex=1 -interaction=nonstopmode -file-line-error main.tex
+
+# bibtex main.aux
 
 texify --pdf --tex-option=--interaction=errorstopmode --tex-option=--synctex=1 $dir/main.tex
 
